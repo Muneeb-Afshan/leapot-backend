922536bd5f4f51ddf172e53bcfba61727894a111
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2024-02-23 20:32:44 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 //config .env file
 require("dotenv").config();
 //config Database
 require("./config/Database").connect();
 const router = require("./routes/AuthenticationRoutes");
 const  routerTeam = require('./routes/TeamRoutes')
+const userDetail = require('./routes/UserDetailsRoutes')
+const utilityRoutes = require('./routes/UtilityRoutes')
 //middleWares
 const bodyParser = require("body-parser");
 const cors = require("cors"); //to handle cors origin error we use cors
@@ -21,8 +25,12 @@ app.use(express.json());
 app.use(cors());
 //Map Router to Path
 app.use("/api", router);
-
 app.use("/api",routerTeam)
+app.use("/api",userDetail)
+app.use("/api",utilityRoutes)
+
+// api document 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //server listen
 app.listen(process.env.PORT, () => {
   console.log(`server is started ${process.env.PORT}`);
