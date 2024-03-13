a5de7c7c5fd3b45ca02f9e39d2cac99e40b55541
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2024-03-13 21:11:47 +0530

    evenyt user logic added

--- controller/eventManager/EventUserController.js
+++ controller/eventManager/EventUserController.js
@@ -1,32 +1,69 @@
 const InstructorModel = require ('../../model/Instructor')
-const UserModel = require ('../../model/EventUserDetailsSchema')
+// const UserModel = require ('../../model/EventUserDetailsSchema')
 
 //adding the users in user detail table
- exports.createUser = async (req, res) => {
-    console.log(req.body);
-    UserModel.create(req.body)
-        .then(user => res.json(user))
-        .catch(err => res.json(err));
-}
+//  exports.createUser = async (req, res) => {
+//     console.log(req.body);
+//     UserModel.create(req.body)
+//         .then(user => res.json(user))
+//         .catch(err => res.json(err));
+// }
+
+const User = require("../../model/EMuserSchema");
+const UserDetails = require('../../model/EMuserDetailSchema')
+//All Authentications rest API are list here
+
+// To add user, admin will add the user
+exports.createUser = async (req, res) => {
+  const { email, role } = req.body;
+  if (!(email && role)) {
+    return res.json({
+      message: "all input feild require",
+    });
+  }
+  const oldUser = await User.findOne({ email: email });
+  if (oldUser) {
+    return res.json({
+      message: "Learner Allready Added in Database",
+      success: false,
+    });
+  }
+
+  const NewUser = new User({
+    email: email,
+    role: role,
+  });
+  NewUser.save();
+
+  const NewUserDetails = new UserDetails({
+    email: email,
+    userid:NewUser._id
+  });
+  NewUserDetails.save();
+  return res.status(201).json({
+    message: "Learner Add successfull",
+    success: true,
+    statsCode:201
+  });
+};
 
 // //fetching the users details
-
 exports.fetchUser = async(req, res) => { 
-    UserModel.find({})
+    User.find({})
     .then(user => res.json (user)) 
     .catch(err => res.json(err))
 }
 
 exports.logicalUserDelete = async(req, res) =>{
     const id = req.params.id;
-    UserModel.findByIdAndDelete({_id: id})
+    User.findByIdAndDelete({_id: id})
     .then(res => res.json (res)) 
     .catch(err => res.json(err))
 }
 // //Updating the User
 exports.fetchUserById = async(req, res) => {
     const id = req.params.id;
-    UserModel.findById({_id:id})
+    User.findById({_id:id})
     .then(user => res.json (user)) 
     .catch(err => res.json(err))
 }
@@ -34,7 +71,7 @@ exports.fetchUserById = async(req, res) => {
 exports.updateUserById = async(req, res) => {
     const id = req.params.id;
     // EventModel.findByIdAndUpdate(id, req.body, { new: true })
-    UserModel.findByIdAndUpdate({_id: id}, {
+    User.findByIdAndUpdate({_id: id}, {
         username: req.body.username,
         email: req.body.email,
         role: req.body.role,
