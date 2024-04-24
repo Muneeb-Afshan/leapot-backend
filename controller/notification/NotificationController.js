05c8cc43f33c7a2f161cfee1b72fed6ec67f10ec
Shreya <101707492+ShreyaC11@users.noreply.github.com>
2024-04-24 09:50:03 +0530

    fetch api for notification

--- controller/notification/NotificationController.js
+++ controller/notification/NotificationController.js
@@ -1,5 +1,5 @@
 const { CreateNotification } = require("../../model/NotificationSchema");
-
+//controller to post notifications
 exports.createNotification = async (req, res) => {
   try {
     const { notificationType, subject, notificationBody } = req.body;
@@ -10,6 +10,16 @@ exports.createNotification = async (req, res) => {
     });
 
     return res.status(200).json(newnotifications);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//controller to fetch notifications
+exports.fetchNotification = async (req, res) => {
+  try {
+    const templatenot = await CreateNotification.find({});
+    return res.status(200).json(templatenot);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/NotificationRoutes.js
+++ routes/NotificationRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const notificationRouter = express.Router();
 const {
   createNotification,
+  fetchNotification,
 } = require("../controller/notification/NotificationController");
 
 notificationRouter.post("/notification/postnotification", createNotification);
+notificationRouter.get("/notification/getnotifications", fetchNotification);
 
 module.exports = notificationRouter;
