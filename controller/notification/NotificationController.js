7ec7046671fc767832c46e6ddf6d7db689e00db9
Shreya <101707492+ShreyaC11@users.noreply.github.com>
2024-04-24 13:57:35 +0530

    update API for notifications

--- controller/notification/NotificationController.js
+++ controller/notification/NotificationController.js
@@ -24,3 +24,28 @@ exports.fetchNotification = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//controller to update existing notifications
+exports.updateNotification = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { notificationType, subject, notificationBody } = req.body;
+    // Update the notification using findByIdAndUpdate
+    const updatednotification = await CreateNotification.findByIdAndUpdate(
+      { _id: id }, // ID to update
+      { notificationType, subject, notificationBody }, // Updated notification data
+      { new: true } // Return the updated document
+    );
+    if (!updatednotification) {
+      return res.status(404).json({ message: "notification not found" });
+    }
+    return res.status(200).send({
+      body: updatednotification,
+      statusCode: 200,
+      message: " Notification updated successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+};
--- routes/NotificationRoutes.js
+++ routes/NotificationRoutes.js
@@ -3,9 +3,14 @@ const notificationRouter = express.Router();
 const {
   createNotification,
   fetchNotification,
+  updateNotification,
 } = require("../controller/notification/NotificationController");
 
 notificationRouter.post("/notification/postnotification", createNotification);
 notificationRouter.get("/notification/getnotifications", fetchNotification);
+notificationRouter.put(
+  "/notification/editnotification/:id",
+  updateNotification
+);
 
 module.exports = notificationRouter;
