985335d8eed08c96b0e190546009cec213a912e2
Diptesh <diptesh.dhangar@leapot.in>
2024-05-03 19:07:01 +0530

    User Module Instructor fields

--- controller/userDetails/UserDetailsController.js
+++ controller/userDetails/UserDetailsController.js
@@ -2,8 +2,8 @@
 const UserDetails = require('../../model/UserDetailsSchema')
 const User = require('../../model/UserSchema');
   exports.GetUserProfileByEmail = async (req, res) =>{
-    const {email , firstname, lastname, ,dateofbirth,bio,country,state ,city,address1,address2,phoneNo,picture } = req.body;
-   console.log("username" , username)
+    const {email , firstname, lastname, dateofbirth,bio,country,state ,city,address1,address2,phoneNo,picture } = req.body;
+  //  console.log("username" , username)
     try {
          
         const oldDetails = await UserDetails.findOne({email:email}).populate('userid')
