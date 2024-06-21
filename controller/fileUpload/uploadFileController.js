56006c6d923385bf00a7d0cad73460f83c66071e
rautshreya <126368318+ShreyaR2409@users.noreply.github.com>
2024-06-21 09:53:15 +0530

    Upload Scrom

--- controller/fileUpload/uploadFileController.js
+++ controller/fileUpload/uploadFileController.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const app = express();
+const http = require('http');
 const AWS = require('aws-sdk');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
-const { v4: uuidv4 } = require('uuid'); // Import UUID generator
-
+const upload = multer({ storage: multer.memoryStorage() });
 
 exports.uploadImages = ('/upload', async (req, res) => {
     console.log("hello")
@@ -107,6 +111,7 @@ try {
 }
 });
 
+
 exports.uploadUserImages = ('/uploadUserImages', async (req, res) => {
   console.log("hello")
   const region = process.env.AWS_REGION;
@@ -176,35 +181,40 @@ try {
 }
 });
 
+
 exports.uploadscrom = ('/uploadscrom', async (req, res) => {
   console.log("hello")
   const region = process.env.AWS_REGION;
   const bucketName = process.env.AWS_BUCKETNAME;
   const accessKeyId = process.env.AWS_ACCESSKEYID;
   const secretAccessKey = process.env.AWS_SECRETACCESSKEY;
-  
-  const s3 = new AWS.S3({
-      region,
-      accessKeyId,
-      secretAccessKey,
-      signatureVersion: "v4"
-  })
 
-// Generate a UUID for the file name
-  const fileName = `${uuidv4()}-${req.body.fileName}`;
-// const fileName = req.body.fileName;
-const fileType = req.body.fileType; // Assuming fileType is sent from frontend
-const params = {
-  Bucket: bucketName,
-  Key: `lms/course/scrom/${fileName}`,
-  ContentType: fileType,
-  Expires: 600
-};
-try {
-  const preSignedUrl = await s3.getSignedUrlPromise('putObject', params);
-  res.json({ url: preSignedUrl });
-} catch (error) {
-  console.error('Error generating pre-signed URL:', error);
-  res.status(500).json({ error: 'Failed to generate pre-signed URL' });
-}
+  const s3 = new AWS.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: "v4"
+  });
+
+  const fileName = req.body.fileName;
+  const fileType = req.body.fileType;
+
+  const folderPath = 'lms/course/scorm/';
+  const filePath = `${folderPath}${fileName}`;
+
+  const params = {
+    Bucket: bucketName,
+    // Key: `lms/course/scorm/${fileName}`,
+    Key: filePath,
+    ContentType: fileType,
+    Expires: 600
+  };
+
+  try {
+    const preSignedUrl = await s3.getSignedUrlPromise('putObject', params);
+    res.json({ url: preSignedUrl, path: folderPath });
+  } catch (error) {
+    console.error('Error generating pre-signed URL:', error);
+    res.status(500).json({ error: 'Failed to generate pre-signed URL' });
+  }
 });
