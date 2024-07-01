60c511add7bad86c58f7387fa85d664cfac7854e
Shreya <101707492+ShreyaC11@users.noreply.github.com>
2024-07-01 13:16:07 +0530

    created quiz module api and controller code

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -1,32 +1,51 @@
-const express = require('express');
-const upload = require('../controller/course/FileUpload');
+const express = require("express");
+const upload = require("../controller/course/FileUpload");
 const courseRouter = express.Router();
-const {createCourse ,fetchCourses , addCourseDetails , fetchCoursesWithDetails , fetchAllCoursesWithDetails , logicalDeleteCourse , createCourseById} = require('../controller/course/CourseController');
-const {addMessage, getMessages, thumbUp, thumbDown} = require('../controller/course/CourseDiscussion');
-const {registerLearner} = require('../controller/course/RegistrationController');
-const {uploadVideo} =require('../controller/fileUpload/uploadFileController')
+const {
+  createCourse,
+  fetchCourses,
+  addCourseDetails,
+  fetchCoursesWithDetails,
+  fetchAllCoursesWithDetails,
+  logicalDeleteCourse,
+  createCourseById,
+} = require("../controller/course/CourseController");
+const {
+  addMessage,
+  getMessages,
+  thumbUp,
+  thumbDown,
+} = require("../controller/course/CourseDiscussion");
+const {
+  registerLearner,
+} = require("../controller/course/RegistrationController");
+const {
+  uploadVideo,
+} = require("../controller/fileUpload/uploadFileController");
+
+const { createQuiz } = require("../controller/course/CourseQuizController");
 
 //  const {  addCourseDetails , getAllCourses} = require('../controller/course/CourseController');
-const verifyToken = require('../middleware/TokenVerifyMiddleware');
+const verifyToken = require("../middleware/TokenVerifyMiddleware");
 
+courseRouter.post("/event/eventRegistration", registerLearner);
+courseRouter.post("/course/createCourse", createCourse);
+courseRouter.post("/course/createCourseById", createCourseById);
+courseRouter.post("/course/addCourseDetails", addCourseDetails);
+courseRouter.get("/course/fetchCourses", fetchCourses);
+courseRouter.get("/course/fetchCoursesWithDetails", fetchCoursesWithDetails);
+courseRouter.get(
+  "/course/fetchAllCoursesWithDetails",
+  fetchAllCoursesWithDetails
+);
+courseRouter.put("/course/logicalDeleteCourse/:id", logicalDeleteCourse);
+courseRouter.post("/course/uploadVideo", uploadVideo);
 
-courseRouter.post('/event/eventRegistration',registerLearner);
-courseRouter.post('/course/createCourse',createCourse);
-courseRouter.post('/course/createCourseById',createCourseById);
-courseRouter.post('/course/addCourseDetails',addCourseDetails);
-courseRouter.get('/course/fetchCourses',fetchCourses);
-courseRouter.get('/course/fetchCoursesWithDetails',fetchCoursesWithDetails);
-courseRouter.get('/course/fetchAllCoursesWithDetails',fetchAllCoursesWithDetails);
-courseRouter.put('/course/logicalDeleteCourse/:id',logicalDeleteCourse);
-courseRouter.post('/course/uploadVideo',uploadVideo);
+courseRouter.post("/course/addMessage", addMessage);
+courseRouter.get("/course/:courseId", getMessages);
+courseRouter.put("/course/messages/:messageId/thumbDown", thumbDown);
+courseRouter.put("/course/messages/:messageId/thumbUp", thumbUp);
 
-
-
-
-courseRouter.post('/course/addMessage',addMessage);
-courseRouter.get('/course/:courseId',getMessages);
-courseRouter.put('/course/messages/:messageId/thumbDown', thumbDown);
-courseRouter.put('/course/messages/:messageId/thumbUp', thumbUp);
-
+courseRouter.post("/course/quizzes", createQuiz);
 
 module.exports = courseRouter;
