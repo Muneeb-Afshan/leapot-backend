d23bbe389c681056df508b604813fc9e2ade7e03
Ganesh Golhar <83921516+ganesh29082002@users.noreply.github.com>
2024-07-17 10:47:58 +0530

    Update CertificateDesign.js

--- controller/certificate/CertificateDesign.js
+++ controller/certificate/CertificateDesign.js
@@ -43,7 +43,7 @@ exports.addTemplate = async (req, res) => {
     });
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json( {error : error.message , message : "Unable to POST Template") };
+    return res.status(500).json( {error : error.message , message : "Unable to POST Template" } ) ;
   }
 };
 
